4052be3dfb2c8b69e25449f18ccea7e5eb25a8a0
Ray Yoon <jaehoonyoon222@gmail.com>
2022-07-04 18:09:40 -0700

    Updating files

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,19 @@
-const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
-
-/*
-const loc = '/data/samples.json'
-data = d3.json(loc);
-console.log(data);
-*/
+//const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 ///*
-
-let dataset = [];
-
 d3.json(url).then(function(data) {
     //console.log(data);
     console.log(data.samples);
-    dataset = data.samples;
 });
 //*/
 
-console.log(dataset);
-
-
-//let dropdownMenu = d3.select('#selDataset');
+/*
+function addData() {
+    let dropdownMenu = d3.select('#selDataset');
+    let dataset = dropdownMenu.property('value');
+    let data = [];
+}
+*/
 
 //let data = d3.json(url);
 //console.log(data);
