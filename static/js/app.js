dae6cc6209cfc8e12a9bd206eae161a2553be2ac
Ray Yoon <jaehoonyoon222@gmail.com>
2022-07-06 16:41:47 -0700

    Updating files

--- static/js/app.js
+++ static/js/app.js
@@ -3,56 +3,51 @@ const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 //defining empty arrays to store data
 let allData = [];
+let subjects = [];
 let values = [];
 let labels = [];
 let hoverText = [];
 
-//using d3 to read in data, then pushing it to arrays
+//using d3 to read in data
 d3.json(url).then(function(data) {
-
     //console.log(data.samples);
 
+    //defining the data array
+    let initData = data.samples[0];
+
+    let trace = {
+        x: initData.sample_values.slice(0, 10),
+        y: initData.otu_ids.slice(0, 10),
+        type: 'bar',
+        text: initData.otu_labels.slice(0, 10),
+        orientation: 'h'
+    };
+
+    let traceData = [trace];
+
+    let layout = {
+        title: 'test',
+        height: 600,
+        width: 800
+    };
+
+    Plotly.newPlot('sample-metadata', traceData, layout);
+
     for (let i = 0; i < data.samples.length; i++) {
-        let row = data.samples[i]
+        let row = data.samples[i];
         //console.log(row);
         //console.log(row.id);
         //console.log(row.otu_ids);
         //console.log(row.otu_labels);
         //console.log(row.sample_values);
-
+        subjects.push(row.id);
         allData.push(row);
         //values.push(row.sample_values.slice(0, 10));
         //labels.push(row.otu_ids.slice(0, 10));
         //hoverText.push(row.otu_labels.slice(0, 10));
-
-    }  
+    };
 });
 console.log(allData);
-
-/*
-//defining the data array
-let trace = {
-    x: values,
-    y: labels,
-    type: 'bar',
-    text: hoverText,
-    orientation: 'h'
-};
-
-let traceData = [trace];
-
-let layout = {
-    title: 'test',
-    margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-    }
-};
-
-Plotly.newPlot('sample-metadata', traceData, layout);
-*/
 
 /*
 d3.selectAll('#selDataset').on('change', addData);
@@ -61,13 +56,23 @@ function addData() {
     let dropdownMenu = d3.select('#selDataset');
     let testSubject = dropdownMenu.property('value');
 
-    let data = allData.find()
-    
-    updatePlotly(data);
-}
+    let data = allData.find();
+    let x = [];
+    let y = [];
+    let text = [];
 
-function updatePlotly(newData) {
-    Plotly.restyle('','',[newData])
+    for (let s = 0; s < subjects.length; s++) {
+        if (testSubject === subjects[s]) {
+            x = '';
+            y = '';
+            text = '';
+        }
+    };
+    
+    Plotly.restyle('sample-metadata','x',[x]);
+    Plotly.restyle('sample-metadata','y',[y]);
+    Plotly.restyle('sample-metadata','text',[text]);
+    
 }
 */
 
