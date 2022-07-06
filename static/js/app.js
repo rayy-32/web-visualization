82048eaf014ada49c55d01355ea25d433fe720ba
Ray Yoon <jaehoonyoon222@gmail.com>
2022-07-05 17:36:39 -0700

    Updating files

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,74 @@
-//const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+//defining the url
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-///*
+//defining empty arrays to store data
+let allData = [];
+let values = [];
+let labels = [];
+let hoverText = [];
+
+//using d3 to read in data, then pushing it to arrays
 d3.json(url).then(function(data) {
-    //console.log(data);
-    console.log(data.samples);
+
+    //console.log(data.samples);
+
+    for (let i = 0; i < data.samples.length; i++) {
+        let row = data.samples[i]
+        //console.log(row);
+        //console.log(row.id);
+        //console.log(row.otu_ids);
+        //console.log(row.otu_labels);
+        //console.log(row.sample_values);
+
+        allData.push(row);
+        //values.push(row.sample_values.slice(0, 10));
+        //labels.push(row.otu_ids.slice(0, 10));
+        //hoverText.push(row.otu_labels.slice(0, 10));
+
+    }  
 });
-//*/
+console.log(allData);
 
 /*
-function addData() {
-    let dropdownMenu = d3.select('#selDataset');
-    let dataset = dropdownMenu.property('value');
-    let data = [];
-}
+//defining the data array
+let trace = {
+    x: values,
+    y: labels,
+    type: 'bar',
+    text: hoverText,
+    orientation: 'h'
+};
+
+let traceData = [trace];
+
+let layout = {
+    title: 'test',
+    margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+    }
+};
+
+Plotly.newPlot('sample-metadata', traceData, layout);
 */
 
-//let data = d3.json(url);
-//console.log(data);
+/*
+d3.selectAll('#selDataset').on('change', addData);
 
-//let values = sample_values
-//let labels = otu_ids
-//let hoverText = otu_labels
+function addData() {
+    let dropdownMenu = d3.select('#selDataset');
+    let testSubject = dropdownMenu.property('value');
 
+    let data = allData.find()
+    
+    updatePlotly(data);
+}
+
+function updatePlotly(newData) {
+    Plotly.restyle('','',[newData])
+}
+*/
 
 //function init() {};
